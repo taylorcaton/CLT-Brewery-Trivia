6b1a88bc2f5b0afea0dd8107ceb5ad2eacc21fea
Taylor Caton <taylorcaton@gmail.com>
2017-06-21 20:37:25 -0400

    dropped carousel for plain divs

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,22 +1,25 @@
-var questions
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        questions = JSON.parse(this.responseText);
-    }
-};
-xmlhttp.open("GET", "assets/js/questions.txt", true);
-xmlhttp.send();
-
+var currentQuestion = 0;
 
 function start(){
-	//Load the questions into the question array
-
 	//Load the quizbox
-	$('.quiz').append("<div class='carousel slide'>");
 
+	//Load the questions into the question array
+	
+		
+}
 
+function getQuestion(){
+	return questions[currentQuestion];
+}
+
+function displayCurrentQuestion(q){
+	$("#questionBox").html("<h1 class='questionText'>"+q.question+"</h1>");
+}
+
+function displayCurrentAnswers(q){
+	for (var i = 0; i < q.choices.length; i++) {
+		$('#answerBox').append("<button class='btn btn-default'>"+q.choices[i]+"</button>");
+	}
 }
 
 start();
---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+ var questions = 
+      [
+        {
+          question: "What is the capital of United Kingdom?",
+          choices: ["Manchester", "Birmingham", "London", "Sussex"],
+          answer: 2
+        },
+        
+        {
+          question: "What is the capital of United States?",
+          choices: ["Chicago", "Washington D.C.", "Miami", "San Francisco"],
+          answer: 1
+        }
+        
+        
+      ];
