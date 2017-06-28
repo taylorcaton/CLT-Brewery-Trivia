9008f14281cf30d6785aa3ed688303d602f79e22
Taylor Caton <taylorcaton@gmail.com>
2017-06-28 10:44:48 -0400

    Added Beer Animation

--- assets/js/beerAnimation.js
+++ assets/js/beerAnimation.js
@@ -1,12 +1,15 @@
 function fillBeer(b){ //if true fill up
   
   var h = "";
+  var r = "";
 
   if(b){
     h="100%"
+    r="-45"
     $("#container").toggle()
   }else {
     h="0%";
+    r="";
   }
 
   $('#liquid') // I Said Fill 'Er Up!
@@ -14,8 +17,25 @@ function fillBeer(b){ //if true fill up
       height: h
     }, 2000);
 
+  $('#logoBox img')
+    .animateRotate(-45,1000);
+
 };
 
 function displayBeer(){
   $("#container").toggle()
 }
+
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+  var args = $.speed(duration, easing, complete);
+  var step = args.step;
+  return this.each(function(i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function(now) {
+      $.style(e, 'transform', 'rotate(' + now + 'deg)');
+      if (step) return step.apply(e, arguments);
+    };
+
+    $({deg: 0}).animate({deg: angle}, args);
+  });
+};
