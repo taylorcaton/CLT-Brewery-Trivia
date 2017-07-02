56b544c10b937733caf85bae13ccea7e1e3d7c15
Taylor Caton <taylorcaton@gmail.com>
2017-07-02 18:41:18 -0400

    shuffle questions and reset timer

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -97,8 +97,11 @@ function displayResults(){
 
 $(document).ready(function(){
 
+	questions = shuffle(questions);
 	updateDisplay();
+	$("#progressbar4").empty();
 	runTimer();
+
 
 
 	$(document).on("click", ".btn-choice", function(){
@@ -127,5 +130,25 @@ function resetGame(){
 	userChoice = -1;
 	score = 0;
 	updateDisplay();
+	$("#progressbar4").empty();
 	runTimer();
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
